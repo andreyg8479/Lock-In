13e9104d0ecbafd929461662c1de4da1f229ea80
DotGyrfalcon <179322305+andreyg8479@users.noreply.github.com>
2026-02-17 12:20:24 -0500

    Started server.js

---
+++ server.js
@@ -0,0 +1,30 @@
+
+const WebSocket = require("ws");
+
+const PORT = process.env.PORT || 8080;
+
+const wss = new WebSocket.Server({ port: PORT });
+
+console.log(`Server running at ws://localhost:${PORT}`);
+
+
+
+//Runs when a browser connects, basicly this is any individual client
+wss.on("connection", (socket) => {
+  console.log("Client connected");
+  
+  //socket.send(); //use this to send stuff to the client
+  
+  //This runs when this client sends a message (CURRENTLY STRINGS)
+  socket.on("message", (message) => {
+    console.log("Received:", message.toString());
+	
+  }
+  
+  
+  
+  
+  socket.on("close", () => {
+    console.log("Client disconnected");
+  });
+});
