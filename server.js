c11f53e9c4d77a1a854b54d85f9bbdcaf2257ba0
DotGyrfalcon <179322305+andreyg8479@users.noreply.github.com>
2026-02-19 09:35:32 -0500

    Outer State Machine Logic

--- server.js
+++ server.js
@@ -7,17 +7,40 @@ const wss = new WebSocket.Server({ port: PORT });
 
 console.log(`Server running at ws://localhost:${PORT}`);
 
-// TODO actually get libary in github
+
+const State = Object.freeze({ //this can be replaced with an actual enum if we switch to typescript
+  STANDARD: 'STANDARD', //for most requests (create note, fetch names, ect)
+  VERIFYING: 'VERIFYING', //When verifying password
+  KEY_SETUP: 'KEY_SETUP', //For setting up verification key
+});
+
 
 //Runs when a browser connects, basicly this is any individual client
 wss.on("connection", (socket) => {
   console.log("Client connected");
+  
+  SM = State.STANDARD; //State Machine
   
   //socket.send(); //use this to send stuff to the client
   
   //This runs when this client sends a message (CURRENTLY STRINGS)
   socket.on("message", (message) => {
     console.log("Received:", message.toString());
+	
+	switch (SM) { //handle the request based on the state
+		case State.STANDARD:
+			//code
+			break;
+		case State.VERIFYING:
+			//code
+			break;
+		case State.KEY_SETUP:
+			//code
+			break;
+		default:
+			console.log("ERROR: Someone is in an undefined state");	
+		
+	}
 	
   });
   
