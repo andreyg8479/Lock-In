6c83a55351aeb1a927fc9a5893dbd8a7dfc9e974
DotGyrfalcon <179322305+andreyg8479@users.noreply.github.com>
2026-02-24 12:58:36 -0500

    NoteEdit Frontend

--- frontend/src/NoteEdit.tsx
+++ frontend/src/NoteEdit.tsx
@@ -1,8 +1,11 @@
-import { useEffect } from 'react' //useState can also be added here but idk if its needed
+import { useEffect, useState } from 'react'
 import { connectSocket, sendMessage } from "./WebSocketConnect";
 import './NoteEdit.css'
 
 function NotePage() {
+	
+	const [title, setTitle] = useState("Untitled Document");
+	const [content, setContent] = useState("");
   
 	useEffect(() => {
 		
@@ -12,18 +15,76 @@ function NotePage() {
 		});
 		
 	}, [])
+	
+	const doSave = () => {
+		const noteName = title; 
+		const noteData = content; //these should be encrypted before sending
+		
+		if (true) { //if its a newly made note vs editing old note
+			sendMessage(JSON.stringify({
+				command: "NewNote",
+				name: noteName,
+				data: noteData
+			}));
+		} else {
+			sendMessage(JSON.stringify({
+				command: "Override",
+				name: noteName,
+				data: noteData
+			}));
+		}
+	}
+	
+	const doCancel = () => {
+		//just go back to the list page
+	}
+	
+	const attachFile = () => {
+		//Not a this week problem
+	}
+	
+	const doDelete = () => {
+		const noteName = title; 
+		if (true) { //if its a note thats been saved before
+			//also make sure the name changing is accounted for
+			sendMessage(JSON.stringify({
+				command: "DeleteNote",
+				name: noteName
+			}));
+		}
+		doCancel();
+	}
 
   return (
 	<div className="note-page">
+	
+		<input
+			className="note-title"
+			type="text"
+			value={title}
+			onChange={(e) => setTitle(e.target.value)}
+		/>
+	
 		<div className="buttons">
 		
-			<button onClick={() => sendMessage("Cancel")}>
+			<button onClick={doCancel}>
 			Cancel
 			</button>
 			
-			<button onClick={() => sendMessage("Save")}>
-			Cancel
+			<button onClick={doSave}>
+			Save
 			</button>
+			
+			<button onClick={attachFile}>
+			Make File
+			</button>
+			
+			<button onClick={doDelete}>
+			Delete
+			</button>
+			
+			{/* more buttons probably */}
+			
 			
 		</div>
 		
@@ -31,6 +92,8 @@ function NotePage() {
 		
 			className="note-text"
 			placeholder="Write note here"
+			value={content}
+			onChange={(e) => setContent(e.target.value)}
 		
 		/>
 	</div>
